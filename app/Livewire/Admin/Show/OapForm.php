2898d187c6f6ec354beadf11a6f9fb40444dd923
Dayoebe <oyetoke.ebenezer@gmail.com>
2026-01-26 09:55:43 +0100

    Made OAP nullable

--- app/Livewire/Admin/Show/OapForm.php
+++ app/Livewire/Admin/Show/OapForm.php
@@ -36,7 +36,7 @@ class OapForm extends Component
                 Rule::unique('oaps', 'staff_member_id')->ignore($this->oapId),
             ],
             'name' => 'required|min:3|max:255',
-            'bio' => 'required|min:10',
+            'bio' => 'nullable|min:10',
             'profile_photo' => 'nullable|url',
             'profile_photo_upload' => 'nullable|image|max:5120',
             'specializations' => 'nullable|string',
